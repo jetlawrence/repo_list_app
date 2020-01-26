50047c91f0731c50ff95f494c6ac7425f16bf39b
Jet Lawrence Doctor <jetlawrence.doctor@gmail.com>
2020-01-26 17:26:07 +0800

    Create repositoriesActions

--- src/actions/repositoriesActions.ts
+++ src/actions/repositoriesActions.ts
@@ -1,8 +1,47 @@
 import IRepository from '../common/types/IRepository';
+import * as repositoriesActionTypes from './actionTypes/repositoriesActionTypes';
 
-export const onRequestRepositories = () => {};
-export const onRequestRepositoriesFailed = (error: string) => {};
-export const onRequestRepositoriesSuccess = () => {};
+interface IFetchRepositoriesRequestAction {
+  type: typeof repositoriesActionTypes.FETCH_REPOSITORIES_REQUEST;
+}
+
+interface IFetchRepositoriesFailureAction {
+  type: typeof repositoriesActionTypes.FETCH_REPOSITORIES_FAILURE;
+  error: string;
+}
+
+interface IFetchRepositoriesSuccessAction {
+  type: typeof repositoriesActionTypes.FETCH_REPOSITORIES_SUCCESS;
+}
+
+interface IUpdateRepositoriesAction {
+  type: typeof repositoriesActionTypes.UPDATE_REPOSITORIES;
+  data: {
+    repositories: Array<IRepository>;
+    totalCount?: number;
+    currentPage?: number;
+  };
+}
+
+export const onRequestRepositories = (): IFetchRepositoriesRequestAction => {
+  return {
+    type: repositoriesActionTypes.FETCH_REPOSITORIES_REQUEST,
+  };
+};
+export const onRequestRepositoriesFailed = (
+  error: string,
+): IFetchRepositoriesFailureAction => {
+  return {
+    type: repositoriesActionTypes.FETCH_REPOSITORIES_FAILURE,
+    error,
+  };
+};
+export const onRequestRepositoriesSuccess = (): IFetchRepositoriesSuccessAction => {
+  return {
+    type: repositoriesActionTypes.FETCH_REPOSITORIES_SUCCESS,
+  };
+};
+
 export const updateRepositories = ({
   repositories,
   currentPage,
@@ -11,4 +50,13 @@ export const updateRepositories = ({
   repositories: Array<IRepository>;
   currentPage?: number;
   totalCount?: number;
-}) => {};
+}): IUpdateRepositoriesAction => {
+  return {
+    type: repositoriesActionTypes.UPDATE_REPOSITORIES,
+    data: {
+      repositories,
+      currentPage,
+      totalCount,
+    },
+  };
+};
