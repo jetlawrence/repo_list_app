8796bf7ffbb5113666a4c15cfac89d327a2b1be3
Jet Lawrence Doctor <jetlawrence.doctor@gmail.com>
2020-01-26 15:16:08 +0800

    Implement actual login and logout functions in authHelper

--- src/helpers/authHelper.ts
+++ src/helpers/authHelper.ts
@@ -1,2 +1,7 @@
-export const login = (email: string, password: string) => {};
-export const logout = () => {};
+import AsyncStorage from '@react-native-community/async-storage';
+
+export const USER_TOKEN_KEY = 'USER_TOKEN_KEY';
+
+export const login = (email: string, password: string) =>
+  AsyncStorage.setItem(USER_TOKEN_KEY, `${email}_${password}`);
+export const logout = () => AsyncStorage.removeItem(USER_TOKEN_KEY);
