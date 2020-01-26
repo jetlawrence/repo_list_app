92aded47005912be768e07fcf12bfd32b21f64dd
Jet Lawrence Doctor <jetlawrence.doctor@gmail.com>
2020-01-26 23:50:12 +0800

    Implement RepoDetailsScreen

--- src/screens/repoDetails/RepoDetailsScreen.tsx
+++ src/screens/repoDetails/RepoDetailsScreen.tsx
@@ -1,10 +1,45 @@
 import React from 'react';
-import { SafeAreaView, Text } from 'react-native';
+import { SafeAreaView, Button, View, StyleSheet, Linking } from 'react-native';
+import { NavigationStackScreenComponent } from 'react-navigation-stack';
+import { IRepository } from '../../common/types';
+import RepoDetailsComponent from './RepoDetailsComponent';
 
-const RepoDetailsScreen = () => (
-  <SafeAreaView>
-    <Text>RepoDetailsScreen</Text>
-  </SafeAreaView>
-);
+const RepoDetailsScreen: NavigationStackScreenComponent = props => {
+  const repository: IRepository = props.navigation.getParam('repository');
+
+  if (!repository) {
+    return null;
+  }
+
+  const onGoToRepo = (url: string) => {
+    Linking.canOpenURL(url).then(supported => {
+      if (supported) {
+        Linking.openURL(url);
+      } else {
+        console.log('Failed to open url: ' + url);
+      }
+    });
+  };
+
+  return (
+    <SafeAreaView>
+      <RepoDetailsComponent repository={repository} />
+      {repository.url && (
+        <View style={styles.goToRepoButtonContainer}>
+          <Button
+            title="Go to repo"
+            onPress={() => onGoToRepo(repository.url)}
+          />
+        </View>
+      )}
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  goToRepoButtonContainer: {
+    margin: 10,
+  },
+});
 
 export default RepoDetailsScreen;
