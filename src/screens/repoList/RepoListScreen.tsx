897021dffc0b8c55d662d02fcc55e98854ac8d0b
Jet Lawrence Doctor <jetlawrence.doctor@gmail.com>
2020-01-26 15:51:29 +0800

    Add logout button in RepoListScreen

--- src/screens/repoList/RepoListScreen.tsx
+++ src/screens/repoList/RepoListScreen.tsx
@@ -1,10 +1,34 @@
 import React from 'react';
-import { SafeAreaView, Text } from 'react-native';
+import { SafeAreaView, Text, StyleSheet } from 'react-native';
+import { logout } from '../../helpers/authHelper';
+import { NavigationInjectedProps } from 'react-navigation';
 
 const RepoListScreen = () => (
   <SafeAreaView>
     <Text>RepoListScreen</Text>
   </SafeAreaView>
 );
+
+RepoListScreen.navigationOptions = ({
+  navigation,
+}: NavigationInjectedProps) => ({
+  headerRight: () => (
+    <Text
+      style={styles.logoutButton}
+      onPress={() => {
+        logout();
+        navigation.navigate('Auth');
+      }}>
+      Logout
+    </Text>
+  ),
+});
+
+const styles = StyleSheet.create({
+  logoutButton: {
+    marginHorizontal: 5,
+    color: 'blue',
+  },
+});
 
 export default RepoListScreen;
