ac787d12dc521e3b59cb190e2c208275589694e9
Jet Lawrence Doctor <jetlawrence.doctor@gmail.com>
2020-01-26 16:21:18 +0800

    Setup redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
+import React from 'react';
 import { createSwitchNavigator, createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import RepoListScreen from './screens/repoList/RepoListScreen';
 import RepoDetailsScreen from './screens/repoDetails/RepoDetailsScreen';
 import LoginScreen from './screens/login/LoginScreen';
 import AuthLoadingScreen from './screens/authLoading/AuthLoadingScreen';
+import { Provider } from 'react-redux';
+import store from './store';
 
 const AppStack = createStackNavigator({
   RepoList: RepoListScreen,
@@ -13,7 +16,7 @@ const AuthStack = createStackNavigator({
   Login: LoginScreen,
 });
 
-export default createAppContainer(
+const Navigation = createAppContainer(
   createSwitchNavigator(
     {
       AuthLoading: AuthLoadingScreen,
@@ -24,4 +27,10 @@ export default createAppContainer(
       initialRouteName: 'AuthLoading',
     },
   ),
+);
+
+export default () => (
+  <Provider store={store}>
+    <Navigation />
+  </Provider>
 );
