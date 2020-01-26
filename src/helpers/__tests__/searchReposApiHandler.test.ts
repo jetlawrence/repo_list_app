f3241e54f8e53ef5bc4bfc7ca44365faee338dad
Jet Lawrence Doctor <jetlawrence.doctor@gmail.com>
2020-01-26 21:39:11 +0800

    Add hasNextPage test in searchReposApiHandler

--- src/helpers/__tests__/searchReposApiHandler.test.ts
+++ src/helpers/__tests__/searchReposApiHandler.test.ts
@@ -32,7 +32,7 @@ describe('searchReposApiHandler', () => {
     fetchMock.resetMocks();
   });
 
-  it('calls https://api.github.com/search/repositories and returns formatted data', async () => {
+  it('calls https://api.github.com/search/repositories', async () => {
     fetchMock.mockResponseOnce(JSON.stringify(mockResponse));
 
     const result = await searchReposApiHandler({
@@ -44,10 +44,54 @@ describe('searchReposApiHandler', () => {
     expect(fetchMock.mock.calls[0][0]).toEqual(
       'https://api.github.com/search/repositories?q=hello&page=2&per_page=50',
     );
+  });
+
+  it('returns formatted data with hasNextPage as true if next page will still have repositorie (given default 50 per page)', async () => {
+    fetchMock.mockResponseOnce(JSON.stringify(mockResponse));
+
+    const result = await searchReposApiHandler({
+      searchTerm: 'hello',
+      page: 2,
+    });
+
     expect(result).toEqual({
       response: {
         page: 2,
         totalCount: 141221,
+        hasNextPage: true,
+        repositories: [
+          {
+            id: 29028775,
+            name: 'react-native',
+            url: 'https://github.com/facebook/react-native',
+            description: 'A framework for building native apps with React.',
+            stargazersCount: 84391,
+          },
+          {
+            id: 32948863,
+            name: 'awesome-react-native',
+            description: 'Awesome react native',
+            url: 'https://github.com/jondot/awesome-react-native',
+            stargazersCount: 26373,
+          },
+        ],
+      },
+    });
+  });
+
+  it('returns formatted data with hasNextPage as false if no more next page (given default 50 per page)', async () => {
+    fetchMock.mockResponseOnce(JSON.stringify(mockResponse));
+
+    const result = await searchReposApiHandler({
+      searchTerm: 'hello',
+      page: 2826,
+    });
+
+    expect(result).toEqual({
+      response: {
+        page: 2826,
+        totalCount: 141221,
+        hasNextPage: false,
         repositories: [
           {
             id: 29028775,
